af6c503cb226e44cf19f5381c7cfef0b7d7c6ee1
Caitlin Settles <csettles@calpoly.edu>
2017-11-29 09:36:35 -0800

    Test pixel read and looping

--- final.asm
+++ final.asm
@@ -31,31 +31,57 @@
 ;r8 is used for X
 
 ;---------------------------------------------------------------------
-init:	 SEI
-         CALL   draw_background         ; draw using default color
+init:	  SEI
+        CALL   draw_background         ; draw using default color
+start_loop:
+        MOV    r7, 0x00                ; generic Y coordinate
+        MOV    r8, 0x00                ; generic X coordinate
+loop_row:
+        MOV    r8,0x00                 ; restart x coordinates
+loop_col:
+        ADD    r8,0x01                 ; increment column count
+        CALL   start_game
+        CMP    r8,0x28                 ; check column is still under 40 (0x28)
+        BRNE   loop_col                ; if it is under 40, keep looping
 
-         MOV    r7, 0x00                ; generic Y coordinate
-         MOV    r8, 0x00                ; generic X coordinate
-         
-loop:
-         CALL   read_pixel
-         AND    r0, 0xFF
-         BREQ   draw_white
-         BRN    draw_black
+        ADD    r7,0x01                 ; increment row count
+        CMP    r7,0x1E                 ; check row is still under 30 (0x1E)
+        BRNE   loop_row                ; branch to draw more rows
+        CALL   delay
+        BRN    start_loop              ; this makes an infinite loop
+
+;--------------------------------------------------------------------
+
+start_game:
+        CALL   read_pixel
+        AND    r0, 0xFF
+        BREQ   draw_white
+        BRN    draw_black
 
 draw_white:
-         MOV r6, 0xFF
-         CALL draw_dot
-         ADD r7
-         BRN loop
+        MOV r6, 0xFF
+        CALL draw_dot
+        RET
 
 draw_black:
-      
+        MOV r6, 0x00
+        CALL draw_dot
+        RET
 
-main:	AND R0, R0
-	
-		BRN    main                    ; continuous loop 
-;--------------------------------------------------------------------
+delay:
+        MOV r1, 0xFF
+d1:
+        MOV r2, 0xFF
+d2:
+        MOV r3, 0xFF
+        
+        SUB r3, 0x01
+        BRNE d2
+        SUB r2, 0x01
+        BRNE d1
+        SUB r1, 0x01
+        BRNE delay
+        RET
 
 
 ;--------------------------------------------------------------------
@@ -92,17 +118,17 @@ draw_horiz1:
 ;-  Tweaked registers: r13,r7,r8,r9
 ;----------------------------------------------------------------------
 draw_background: 
-         MOV   r6,BG_COLOR              ; use default color
-         MOV   r13,0x00                 ; r13 keeps track of rows
-start:   MOV   r7,r13                   ; load current row count 
-         MOV   r8,0x00                  ; restart x coordinates
-         MOV   r9,0x27 
+        MOV   r6,BG_COLOR              ; use default color
+        MOV   r13,0x00                 ; r13 keeps track of rows
+start:  MOV   r7,r13                   ; load current row count 
+        MOV   r8,0x00                  ; restart x coordinates
+        MOV   r9,0x27 
  
-         CALL  draw_horizontal_line
-         ADD   r13,0x01                 ; increment row count
-         CMP   r13,0x1E                 ; see if more rows to draw
-         BRNE  start                    ; branch to draw more rows
-         RET
+        CALL  draw_horizontal_line
+        ADD   r13,0x01                 ; increment row count
+        CMP   r13,0x1E                 ; see if more rows to draw
+        BRNE  start                    ; branch to draw more rows
+        RET
 ;---------------------------------------------------------------------
     
 ;---------------------------------------------------------------------
@@ -115,20 +141,21 @@ start:   MOV   r7,r13                   ; load current row count
 ;- Tweaked registers: r4,r5
 ;---------------------------------------------------------------------
 draw_dot: 
-           MOV   r4,r7         ; copy Y coordinate
-           MOV   r5,r8         ; copy X coordinate
+        MOV   r4,r7         ; copy Y coordinate
+        MOV   r5,r8         ; copy X coordinate
 
-           AND   r5,0x3F       ; make sure top 2 bits cleared
-           AND   r4,0x1F       ; make sure top 3 bits cleared
-           LSR   r4             ; need to get the bot 2 bits of r4 into sA
-           BRCS  dd_add40
-t1:        LSR   r4
-           BRCS  dd_add80
+        AND   r5,0x3F       ; make sure top 2 bits cleared
+        AND   r4,0x1F       ; make sure top 3 bits cleared
+        LSR   r4             ; need to get the bot 2 bits of r4 into sA
+        BRCS  dd_add40
+t1:     LSR   r4
+         
+        BRCS  dd_add80
 
-dd_out:    OUT   r5,VGA_LADD   ; write bot 8 address bits to register
-           OUT   r4,VGA_HADD   ; write top 3 address bits to register
-           OUT   r6,VGA_COLOR  ; write data to frame buffer
-           RET
+dd_out: OUT   r5,VGA_LADD   ; write bot 8 address bits to register
+        OUT   r4,VGA_HADD   ; write top 3 address bits to register
+        OUT   r6,VGA_COLOR  ; write data to frame buffer
+        RET
 
 dd_add40:  OR    r5,0x40       ; set bit if needed
            CLC                  ; freshen bit
@@ -139,29 +166,31 @@ dd_add80:  OR    r5,0x80       ; set bit if needed
 ; --------------------------------------------------------------------
 
 ;---------------------------------------------------------------------
-;- Subrountine: draw_dot
+;- Subrountine: read_pixel
 ;- 
-;- This subroutine draws a dot on the display the given coordinates: 
+;- This subroutine will read a pixel color from the 2k RAM: 
 ;- 
-;- (X,Y) = (r8,r7)  with a color stored in r6  
+;- (X,Y) = (r8,r7)
 ;- 
 ;- Tweaked registers: r4,r5
 ;---------------------------------------------------------------------
 read_pixel: 
-           MOV   r4,r7         ; copy Y coordinate
-           MOV   r5,r8         ; copy X coordinate
+        MOV   r4,r7         ; copy Y coordinate
+        MOV   r5,r8         ; copy X coordinate
 
-           AND   r5,0x3F       ; make sure top 2 bits cleared
-           AND   r4,0x1F       ; make sure top 3 bits cleared
-           LSR   r4             ; need to get the bot 2 bits of r4 into sA
-           BRCS  dd_add40
-t1:        LSR   r4
-           BRCS  dd_add80
+        AND   r5,0x3F       ; make sure top 2 bits cleared
+        AND   r4,0x1F       ; make sure top 3 bits cleared
+        LSR   r4            ; need to get the bot 2 bits of r4 into sA
+        BRCS  dd_add40      ; spend more time with alex
+                            ; he's pretty cute
+t1:     LSR   r4            ; and so are you
+                            ; we should go out sometime
+        BRCS  dd_add80
 
-dd_out:    OUT   r5,VGA_LADD   ; write bot 8 address bits to register
-           OUT   r4,VGA_HADD   ; write top 3 address bits to register
-           IN    r0,VGA_READ
-           RET
+dd_out: OUT   r5,VGA_LADD   ; write bot 8 address bits to register
+        OUT   r4,VGA_HADD   ; write top 3 address bits to register
+        IN    r0,VGA_READ
+        RET
 
 dd_add40:  OR    r5,0x40       ; set bit if needed
            CLC                  ; freshen bit
